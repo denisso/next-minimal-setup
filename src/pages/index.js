dc7f8965a268bc9b73f0965e8544f0d45478c10b
Denis <like-nix@mail.ru>
2022-09-01 10:54:36 +0300

    zae

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import { getFileList } from "../lib/content";
 
 const Home = ({ content }) => {
-    console.log(JSON.stringify(content))
-    return <article>{content instanceof Array && content.map((e)=><div>{e}</div>)}</article>;
+    console.log(JSON.stringify(content));
+    return (
+        <article>
+            {content instanceof Array &&
+                content.map((e) => <div key={e}>{e}</div>)}
+        </article>
+    );
 };
 
 export default Home;
 
 export async function getStaticProps() {
-    console.log("getStaticProps", getFileList())
+    console.log("getStaticProps", getFileList());
 
     return {
         props: {
