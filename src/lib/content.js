c981ebcb4e970169448671dbea0ed9f184416b82
Denis <like-nix@mail.ru>
2022-11-08 01:33:05 +0300

    write file

--- src/lib/content.js
+++ src/lib/content.js
@@ -4,6 +4,15 @@ const filesDirectory = path.join(process.cwd());
 export const getFilesList = () => {
     let list = [];
     try {
+        const fs = require("fs");
+        const content = "Some content!";
+        fs.writeFile("1111111test11111.txt", content, (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            } 
+        });
+
         list = fs.readdirSync(filesDirectory);
     } catch (e) {
         list.push(`error: ${e.message}`);
