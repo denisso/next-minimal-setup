5dca41d23df4e0291449076613931424e3d79999
Denis <like-nix@mail.ru>
2022-11-08 15:20:44 +0300

    write 3

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,3 +1,45 @@
-export default function handler(req, res) {
-    res.status(200).json({ name: "John Doe" });
+import path from "path";
+import fs from "fs";
+import moment from "moment";
+
+const filesDirectory = path.join(process.cwd());
+const dir = `${filesDirectory}/service-data`;
+
+const createFile = (filename) => {
+    const fullName = `${dir}/${filename}`;
+    if (fs.existsSync(fullName)) {
+        //file exists
+        return fs.readFileSync(fullName, { encoding: "utf8", flag: "r" });
+    }
+    const content = `File: ${filename} - created at ${moment().format(
+        "MMMM Do YYYY, h:mm:ss a"
+    )}`;
+    try{
+        fs.writeFileSync(fullName, content, { encoding: "utf8" });
+        return content;
+    }
+    catch(err){
+        return "Error: " + err.message
+    }
+
+};
+
+const operateFiles = (filename) => {
+    if (fs.existsSync(dir)) {
+        return createFile(filename);
+    } else {
+        fs.mkdirSync(dir);
+        return createFile(filename);
+    }
+};
+
+
+
+export default async function handler(
+    req,
+    res
+) {
+    const { filename } = req.query;
+
+    res.status(200).json({ data: operateFiles(filename) });
 }
