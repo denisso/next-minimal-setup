62fe5243aa60222487dc18d88839f6039b40f43e
Denis <like-nix@mail.ru>
2022-11-08 22:14:46 +0300

    write 46

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -3,9 +3,12 @@ import axios from "axios";
 const fetch = (filename) =>
     new Promise((resolve, reject) => {
         axios
-            .post("http://localhost:8888/.netlify/functions/file-read-write", {
-                filename,
-            })
+            .post(
+                `${process.env.HOSTNAME}/.netlify/functions/file-read-write`,
+                {
+                    filename,
+                }
+            )
             .then(({ data }) => {
                 resolve(data);
             })
@@ -16,8 +19,9 @@ const fetch = (filename) =>
 
 export default function handler(req, res) {
     const { filename } = req.body;
-
-    if(!typeof filename === "string") return res.status(200).json({ data: "filename not defined" })
+    console.log("hostname", process.env.HOSTNAME);
+    if (!typeof filename === "string")
+        return res.status(200).json({ data: "filename not defined" });
     fetch(filename)
         .then((data) => res.status(200).json({ data }))
         .catch((err) => res.status(200).json({ data: err.message }));
