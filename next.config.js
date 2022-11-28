f5930962746d690c9b22f8ba7f9b1281e2b32f43
Denis <like-nix@mail.ru>
2022-11-28 19:34:28 +0300

    test images

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 module.exports = {
+    images: {
+        domains: ["res.cloudinary.com"],
+    },
+
     webpack: (cfg) => {
-        cfg.module.rules.push(
-            {
-                test: /\.md$/,
-                loader: 'frontmatter-markdown-loader',
-                options: { mode: ['react-component'] }
-            }
-        )
+        cfg.module.rules.push({
+            test: /\.md$/,
+            loader: "frontmatter-markdown-loader",
+            options: { mode: ["react-component"] },
+        });
         return cfg;
-    }
-}
+    },
+};
---
+++ src/components/Component.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+export const Component = () => {
+    return (
+        <>
+            <div>
+                <Image
+                    src="https://res.cloudinary.com/mrdramm/image/upload/v1659598817/cld-sample-5.jpg"
+                    width="600"
+                />{" "}
+            </div>
+        </>
+    );
+};
